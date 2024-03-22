ca90175a964f0e9d70861ecf718eb90e34b58822
sipup <valeraakatev2@gmail.com>
2024-03-22 19:28:57 +0300

    add catalog cubit

--- lib/features/app/root_screen.dart
+++ lib/features/app/root_screen.dart
@@ -1,6 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pet/features/app/router.dart';
+import 'package:pet/features/catalog/data/catalog_repository.dart';
+import 'package:pet/features/catalog/domain/catalog_cubit.dart';
+import 'package:pet/features/catalog/domain/catalog_state.dart';
 
 @RoutePage()
 class RootScreen extends StatelessWidget {
@@ -8,34 +12,40 @@ class RootScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AutoTabsScaffold(
-      routes: const [
-        HomeRoute(),
-        CatalogRoute(),
-        CartRoute(),
-        ProfileRoute(),
-      ],
-      bottomNavigationBuilder: (context, tabsRouter) => NavigationBar(
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.home),
-            label: 'Главная',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.manage_search),
-            label: 'Каталог',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.shopping_cart),
-            label: 'Корзина',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.person),
-            label: 'Профиль',
-          ),
+    return BlocProvider(
+      create: (context) => CatalogCubit(
+        const CatalogState(product: [], isLoading: true),
+        CatalogRepositoryImpl(),
+      ),
+      child: AutoTabsScaffold(
+        routes: const [
+          HomeRoute(),
+          CatalogRoute(),
+          CartRoute(),
+          ProfileRoute(),
         ],
-        selectedIndex: tabsRouter.activeIndex,
-        onDestinationSelected: tabsRouter.setActiveIndex,
+        bottomNavigationBuilder: (context, tabsRouter) => NavigationBar(
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Icons.home),
+              label: 'Главная',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.manage_search),
+              label: 'Каталог',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.shopping_cart),
+              label: 'Корзина',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.person),
+              label: 'Профиль',
+            ),
+          ],
+          selectedIndex: tabsRouter.activeIndex,
+          onDestinationSelected: tabsRouter.setActiveIndex,
+        ),
       ),
     );
   }
